191130be6df5f1e958ae03cd3fb3fb65600e0918
abie115 <donicczka@gmail.com>
2016-03-04 02:26:07 +0100

    update print and readme

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ mvn jetty:run
 
 projekt:
 zaimportowanie zadanie2-jvm do IDE (Eclipse) i uruchomienie Main.java
+<br>
+  <br>
+
+Własny Classloader, który dynamicznie ładuje klasę, pobieraną z serwisu RESTowego.<br>
+Dynamiczne podmienianie plików class.<br>
+"hello message 2" pochodzi z klasy z resta, "hello" z  klasy projektu zadanie2-jvm.
+```
+Ss1: com.abiewska.jvm.CustomClassLoader@33909752
+Ss2: sun.misc.Launcher$AppClassLoader@73d16e93
+1. hello message 2
+2. hello
+
+1. hello message 2
+2. hello
+...
+```
--- zadanie2-jvm/src/com/abiewska/jvm/Main.java
+++ zadanie2-jvm/src/com/abiewska/jvm/Main.java
@@ -6,10 +6,12 @@ public class Main {
 			InterruptedException {
 		Service s1, s2;
 		s1 = ServiceFactory.newInstance();
-		System.out.println("Ss2: " + s1.getClass().getClassLoader());
-
+		s2 = new ServiceImpl();
+		
+		System.out.println("Ss1: " + s1.getClass().getClassLoader());
+		System.out.println("Ss2: " + s2.getClass().getClassLoader());
+		
 		while (true) {
-			s2 = ServiceFactory.newInstance();
 			System.out.println("1. " + s1.helloMessage());
 			System.out.println("2. " + s2.helloMessage());
 			System.out.println();
